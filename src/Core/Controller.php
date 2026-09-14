df4309bc7a478a69d02c785c3622150145497764
Michael Laau <michael.laau2401@gmail.com>
2026-09-14 03:48:31 +0200

    feat(core): add base Controller class and view structure

---
+++ src/Core/Controller.php
@@ -0,0 +1,17 @@
+<?php 
+
+abstract class Controller{
+
+    protected function isLoggedIn(){
+        return isset($_SESSION['user_id']);
+    }
+
+    protected function render(string $view, array $data = []){
+        extract($data);
+        
+        ob_start();
+        require __DIR__ . '/../View/' . $view . '.php';
+        $content = ob_get_clean();
+        require __DIR___ . '/../View/layout/main.php'; 
+    }
+}
